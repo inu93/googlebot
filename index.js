2c303ab71bc73c21b85b8bc1f8c336eb0c185802
inu93 <82743280+inu93@users.noreply.github.com>
2021-08-16 21:42:24 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const axios = require('axios')
 const n = new Telegraf(process.env.token)
 
 n.command('start', (ctx) => {
-  ctx.reply('Hai '+ctx.from.first_name+' 🤖 Saya adalah bot yang di buat untuk mencari sesuatu dari google.
-Kirimkan kata/keyword yang ingin anda cari.')
+  ctx.reply('Hai '+ctx.from.first_name+' Saya Adalah Bot Google,Silahkan Ketik Kata/Keyword Yang Mau Kamu Cari')
 
 n.on('message', (ctx) => { `  `
     let input = ctx.message.text;
