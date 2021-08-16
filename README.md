5ae879ac36da42f35d92fbe73edf2874b1901148
root <root@production-builder.us-west1-b.c.railway-infra.internal>
2021-08-16 13:55:24 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Google Bot
+Google Search Bot For Telegram Written With NodeJs
+
+# FrameWork
+[`Telegraf`](https://github.com/telegraf/telegraf)
+
+# Heroku
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy?template=https://github.com/Nekozu/google-bot.git)
+
+# Replit
+
+[![replit deploy](https://replit.com/badge/github/nekozu/google-bot)](https://repl.it/github/nekozu/google-bot)
+
+# Railway
+[![Deploy on Railway](https://railway.app/button.svg)](https://railway.app/new/template?template=https%3A%2F%2Fgithub.com%2Fnekozu%2Fgoogle-bot&envs=token&tokenDesc=bot+token+mu)
---
+++ index.js
@@ -0,0 +1,25 @@
+const { Telegraf } = require("telegraf");
+const axios = require('axios')
+
+const n = new Telegraf(process.env.token)
+
+n.command('start', (ctx) => {
+  ctx.reply('Hai '+ctx.from.first_name+' Silahkan Ketik Kata Yang Mau Kamu Cari')
+
+n.on('message', (ctx) => { `  `
+    let input = ctx.message.text;
+    axios.get('https://google-api.xlaaf.repl.co/search?q='+input)
+    .then(res => {
+         const me = res.data.data
+         const hai = me[Math.floor(Math.random() * (me.length))] 
+         const judul = hai.title
+         const link = hai.link
+         const desk = hai.desk
+         ctx.reply(`Ditemukan : ${input}\n${judul}\nUrl: ${link}\nDeskripsi: ${desk}`)
+       })
+    }).catch(e => {
+         console.log(e);
+   })
+})
+
+n.launch()
